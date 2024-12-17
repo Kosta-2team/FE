39bea65946394352ad6d073066dd27bef2933254
이현경 <leehyunkyung@ihyeongyeong-ui-MacBookAir.local>
2024-12-17 13:41:41 +0900

    파일병합

---
+++ pages/ResetPassword.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import styles from '../styles/reset-password.module.css'
+
+export default function ResetPassword() {
+  const router = useRouter();
+  const { id } = router.query; // ID 전달받음
+  const [newPw, setNewPw] = useState('');
+  const [confirmPw, setConfirmPw] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newPw !== confirmPw) {
+      setError('두 값이 일치하지 않습니다.');
+      return;
+    }
+
+    const res = await fetch('/api/auth/reset-password', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id, newPw }),
+    });
+    const data = await res.json();
+
+    if (data.error) {
+      setError(data.error);
+    } else {
+      alert('비밀번호가 변경되었습니다.');
+      router.push('/Login');
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+      <img src='/logo_nonefill.svg' alt='logo' className={styles.logo}></img>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div>
+          <label htmlFor="newPw"></label>
+          <input
+            className={styles.input}
+            type="password"
+            id="newPw"
+            value={newPw}
+            onChange={(e) => setNewPw(e.target.value)}
+            placeholder='새 비밀번호를 입력하세요.'
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPw"></label>
+          <input
+            className={styles.input}
+            type="password"
+            id="confirmPw"
+            value={confirmPw}
+            onChange={(e) => setConfirmPw(e.target.value)}
+            placeholder='새 비밀번호를 다시 입력하세요.'
+            required
+          />
+          <p className={styles.subtext}>영문, 숫자, 특수문자 (~!@#$%^&*)로 구성해주세요.</p>
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit" className={styles.btn}>비밀번호 변경</button>
+      </form>
+    </div>
+  </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
+import { SessionProvider } from 'next-auth/react';
 
-import NavBar from "@/components/NavBar";
-import Footer from "@/components/Footer";
-import '../styles/reset.css';
-import "@/styles/globals.css";
-
-
-
-/*공통 컴포넌트 레이아웃*/
-export default function App({ Component, pageProps }) {
-  return (<>
-  <NavBar />
-  <Component {...pageProps}/>
-  <Footer /> 
-  </>);
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
